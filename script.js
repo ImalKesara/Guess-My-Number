945ce0bd37c7f32e2fd09f41a21114cf8b830388
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2023-12-31 12:25:39 +0530

    change "" to ''

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const guess = document.querySelector(".message");
+console.log(guess.textContent);
+
+document.querySelector(".message").textContent = "let's go";
+document.querySelector(".number").textContent = 30;
+document.querySelector(".score").textContent = 30;
